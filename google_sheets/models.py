97b532661b6197a062a150a1a0c2b1bcaf1460de
Bakhytbek <baxa.aseke@gmail.com>
2022-09-12 09:03:35 +0600

    # Реализовал вэб страницу

--- google_sheets/models.py
+++ google_sheets/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class GoogleSheets(models.Model):
+    number = models.BigIntegerField(primary_key=True, unique=True, verbose_name="№")
     order_number = models.CharField(
-        max_length=200, null=False, blank=False,
+        max_length=200, null=True, blank=False,
         verbose_name="заказ №")
     price = models.CharField(
-        max_length=200, null=False, blank=False,
+        max_length=200, null=True, blank=False,
         verbose_name="стоимость,$")
-    date = models.DateField(null=False, blank=False, verbose_name="срок поставки")
+    price_rubles = models.CharField(
+        max_length=200, null=True, blank=False,
+        verbose_name="стоимость в рублях")
+    date = models.DateField(null=True, blank=False, verbose_name="срок поставки")
--- google_sheets/views.py
+++ google_sheets/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from task123.sheets import Sheets
+from google_sheets.models import GoogleSheets
+from datetime import datetime
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        sheets = Sheets()
+        GoogleSheets.objects.all().delete()
+        for data in sheets.get_data():
+            GoogleSheets.objects.create(number=data[0], order_number=data[1],
+                                        price=data[2], date=datetime.strptime(data[3], '%d.%m.%Y'), price_rubles=data[4])
+        return render(request, self.template_name, context={'sheets': GoogleSheets.objects.all()})
